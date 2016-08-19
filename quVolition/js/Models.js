25fb7341c3a43c37eb360d9ad4319df61ba2a4c5
hakga <sone@fa2.so-net.ne.jp>
2016-08-19 18:27:51 +0900

    デザイン修正

--- quVolition/js/Models.js
+++ quVolition/js/Models.js
@@ -1,3 +1,5 @@
+/// <reference path="jquery.customSelect.min.js">
+
 var viewModel = function( partitions, members) {
 	var self = this;
 	this.partitions = ko.mapping.fromJS( partitions);
@@ -5,19 +7,52 @@ var viewModel = function( partitions, members) {
 	this.partitionId = ko.observable( partitions[0].Id || 0);
 	this.partitionBy = ko.observable(3);
 	this.onPartition = function (o, e) {
-	    self.partitionId(o.Id());
+		self.partitionId(o.Id());
 	}.bind(this);
 	this.listPartitions = ko.computed( function() {
-	    return self.partitions();
+		return self.partitions.sort( function (left,right) {
+			return left.term()==right.term()?0:(left.term()<right.term()?-1:1);
+		}).slice( 0, self.partitionBy()+1);
 	});
+	this.morePartitions = function (o, e) {
+	    self.partitionBy(self.partitionBy() + 2);
+	}.bind(this);
+	this.newPartition = function (o, e) {
+	    var pTerm = new Date();
+	    pTerm.setDate(pTerm.getDate() + 14);
+	    self.partitions.push(ko.mapping.fromJS({ "Id": 0, "name": "", "description": "", "sections": [], "guests": [], "options": [], "term": pTerm.toISOString() }));
+		self.partitionId(0);
+	}.bind(this);
 	this.addPartition = function (o, e) {
-	    var obj = o;
+		var pData = self.partitions()[self.Idx()];
+		var pUrl = 'api/Partitions/';
+		var pType;
+		if ( self.partitionId() == 0) {
+			pType =  'POST';
+		} else {
+			pType =  'PUT';
+			pUrl += self.partitionId();
+		}
+		$.ajax ({
+			url: pUrl,
+			type: pType,
+			scriptCharset:'utf-8',
+			data : {name:pData.name(), description:pData.description(), options:pData.options(), sections:pData.sections(), guests:pData.guests(), term:pData.term()},
+			dataType: 'json'
+		}).done( function( json) {
+			alert('登録完了');
+		}).fail( function() {
+			alert('登録失敗');
+		});
+	}.bind(this);
+	this.delPartition = function (o, e) {
 	}.bind(this);
 	this.addSection = function (o, e) {
-		var obj = o;
+		self.partitions()[self.Idx()].sections.push("");
 	}.bind(this);
 	this.delSection = function (o, e) {
-	    var obj = o;
+	    var idx = $(e.target).data('section');
+		self.partitions()[self.Idx()].sections.splice(idx, 1) ;
 	}.bind(this);
 	this.Idx = function () {
 		for ( var i = 0; i < self.partitions().length; i++) {
@@ -67,6 +102,9 @@ var viewModel = function( partitions, members) {
 		}
 		return "";	//no data
 	};
+	this.guested = ko.computed(function () {
+	    return self.partitions()[self.Idx()].guests();
+	})
 	this.onChange = function( e) {	// event fired with jQuery
 		var tr =  $(e.target).parents('tr')[0];
 		var guestid = $(tr).data('guestid');
