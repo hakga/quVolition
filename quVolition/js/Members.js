0dbdfad84168d4a45bd12b856a0f33ad3053ee91
hakga <sone@fa2.so-net.ne.jp>
2016-09-18 17:00:40 +0900

    Members.js入れ忘れ

---
+++ quVolition/js/Members.js
@@ -0,0 +1,12 @@
+var MemberList = [
+{ "Id": "001", "name": "イワン", "mail": "cyborg001@ish.pro", "group": [0, 10] },
+{ "Id": "002", "name": "ジェット", "mail": "cyborg002@ish.pro", "group": [0, 11] },
+{ "Id": "003", "name": "フランソワーズ", "mail": "cyborg003@ish.pro", "group": [0, 10] },
+{ "Id": "004", "name": "ハインリヒ", "mail": "cyborg004@ish.pro", "group": [0, 10] },
+{ "Id": "005", "name": "ジェロニモ", "mail": "cyborg005@ish.pro", "group": [0, 11] },
+{ "Id": "006", "name": "張々湖", "mail": "cyborg006@ish.pro", "group": [0, 12] },
+{ "Id": "007", "name": "ブリテン", "mail": "cyborg007@ish.pro", "group": [0, 10] },
+{ "Id": "008", "name": "ピュンマ", "mail": "cyborg008@ish.pro", "group": [0, 13] },
+{ "Id": "009", "name": "島村ジョー", "mail": "cyborg009@ish.pro", "group": [0, 12] },
+{ "Id": "100", "name": "ギルモア", "mail": "human100@ish.pro", "group": [1, 10] }
+];
